93921879ee7e78dbd9fcb95dd09ae68ea23e26b8
DevExpressExampleBot <fakeEmail>
2021-05-04 13:54:04 +0300

    Source auto update [skip ci]

---
+++ VB/WpfApplication28/MainWindow.xaml.vb
@@ -0,0 +1,37 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports System.Data
+
+Namespace WpfApplication28
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim table As New DataTable()
+			table.Columns.Add("Subject", GetType(String))
+			table.Columns.Add("IsRead", GetType(Boolean))
+			table.Rows.Add(New Object() {"Mail1", True})
+			table.Rows.Add(New Object() {"Mail2", False})
+
+			gridControl1.ItemsSource = table
+		End Sub
+	End Class
+End Namespace
